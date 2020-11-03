ef6f2d348c90b2a2b7d05eca0ac02574735b1725
Andrew Lamoureux <andrew@vector35.com>
2020-11-03 16:47:45 -0500

    Add disassembler mode for speed test

--- disassembler/test.c
+++ disassembler/test.c
@@ -3,7 +3,7 @@
 // b aarch64_decompose
 // b aarch64_disassemble
 //
-// gcc -ofast test.c aarch64.c -o test
+// gcc -ofast test.c arm64dis.c -o test
 // time ./test speed
 
 #include <stdio.h>
@@ -11,29 +11,43 @@
 #include <stdint.h>
 #include "arm64dis.h"
 
-int main(int ac, char **av)
+int disassemble(uint64_t address, uint32_t insword, char *result)
 {
-	uint32_t insword = strtoul(av[1], NULL, 16);
-	uint32_t address = 0;
-	uint32_t rc;
-
+	int rc;
 	Instruction instr;
 	memset(&instr, 0, sizeof(instr));
 
 	rc = aarch64_decompose(insword, &instr, address);
 	if(rc) {
-		printf("ERROR: aarch64_decompose() returned %d\n", rc);
+		//printf("ERROR: aarch64_decompose() returned %d\n", rc);
 		return rc;
 	}
 
-	char instxt[4096];
-	memset(instxt, 0, sizeof(instxt));
-	rc = aarch64_disassemble(&instr, instxt, sizeof(instxt));
+	rc = aarch64_disassemble(&instr, result, 1024);
 	if(rc) {
-		printf("ERROR: aarch64_disassemble() returned %d\n", rc);
+		//printf("ERROR: aarch64_disassemble() returned %d\n", rc);
 		return rc;
 	}
 
-	printf("%08X: %s\n", address, instxt);
+	return 0;
+}
+
+int main(int ac, char **av)
+{
+	char instxt[1024];
+
+	if(ac > 1 && !strcmp(av[1], "speed")) {
+		srand(0xCAFE);
+		for(int i=0; i<100000000; i++) {
+			uint32_t insword = (rand() << 16) ^ rand();
+			disassemble(0, insword, instxt);
+			//printf("%08X: %s\n", insword, instxt);
+		}
+	}
+	else {
+		uint32_t insword = strtoul(av[1], NULL, 16);
+		disassemble(0, insword, instxt);
+		printf("%08X: %s\n", insword, instxt);
+	}
 }
 
